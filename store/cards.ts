82080009366b20e65cb092ec27d8281146aa8da0
joaosilvalopes <jpgdsl@gmail.com>
2022-07-12 07:33:01 +0100

    feat: Add error handeling

--- store/cards.ts
+++ store/cards.ts
@@ -1,23 +1,43 @@
 import { atom, useRecoilValue, useSetRecoilState } from "recoil"
+import Swal from "sweetalert2"
 
-const defaultValue: {
-    [key: string]: string[]
-} = {}
+const defaultState: {
+    error?: string,
+    value: {
+        [key: string]: string[]
+    }
+} = {
+    error: undefined,
+    value: {}
+}
 
 const cardsState = atom({
     key: "cardsState",
-    default: defaultValue,
+    default: defaultState,
+    effects: [({ onSet }) => onSet(({ error }) => error && Swal.fire({
+        title: 'Error!',
+        text: error,
+        icon: 'error',
+        confirmButtonText: 'OK'
+    }))]
 })
 
 export const useAddCard = (status: string) => {
     const setCards = useSetRecoilState(cardsState)
 
-    return (card: string) =>
-        setCards((prev) => ({
-            ...prev,
-            [status]: [...(prev[status] || []), card],
-        }))
+    return (card: string) => setCards((prev) => {
+        const cards = prev.value[status] || [];
+
+        if(card === "") {
+            return { error: 'Invalid Card: Empty Value', value: prev.value  }
+        }
+        if(cards.includes(card)) {
+            return { error: 'Invalid Card: Repeated Value', value: prev.value  }
+        }
+
+        return { error: undefined, value: { ...prev.value, [status]: [...cards, card] }};
+    });
 }
 
 export const useCards = (status: string) =>
-    useRecoilValue(cardsState)[status] || []
+    useRecoilValue(cardsState).value[status] || []
--- store/statuses.ts
+++ store/statuses.ts
@@ -1,14 +1,35 @@
 import { atom, useRecoilValue, useSetRecoilState } from "recoil"
+import Swal from "sweetalert2"
+
+const defaultState: { error?: string, value: string[] } = {
+    error: undefined,
+    value: ["todo", "done"]
+};
 
 const statusesState = atom({
     key: "statusesState",
-    default: ["todo", "done"],
+    default: defaultState,
+    effects: [({ onSet }) => onSet(({ error }) => error && Swal.fire({
+        title: 'Error!',
+        text: error,
+        icon: 'error',
+        confirmButtonText: 'OK'
+    }))]
 })
 
 export const useAddStatus = () => {
     const setStatuses = useSetRecoilState(statusesState)
 
-    return (status: string) => setStatuses((prev) => [...prev, status])
+    return (status: string) => setStatuses((prev) => {
+        if(status === "") {
+            return { error: 'Invalid Status: Empty Value', value: prev.value  }
+        }
+        if(prev.value.includes(status)) {
+            return { error: 'Invalid Status: Repeated Value', value: prev.value  }
+        }
+
+        return { error: undefined, value: [...prev.value, status] };
+    });
 }
 
-export const useStatuses = () => useRecoilValue(statusesState)
+export const useStatuses = () => useRecoilValue(statusesState).value
